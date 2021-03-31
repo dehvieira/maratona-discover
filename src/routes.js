e8bc8e7bc6c3ef7b903704f33571d094e7ce1d2f
dehveira <andrevieira_@outlook.com>
2021-03-31 01:04:09 -0300

    maratona-discovery aula-1

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express') //lib express 
+const routes = express.Router() //criando rotas
+
+const views = __dirname + "/views/"
+
+const profile = {
+  name: "André Vieira",
+  avatar: "https://avatars.githubusercontent.com/u/27936380?s=400&u=a9f9fdb7020eeabd84c2ab1bbfb598216a4e99c4&v=4",
+  "monthly-budget": 3000,
+  "hours-per-day": 6,
+  "days-per-week": 5,
+  "vacation-per-year" : 4
+}
+
+//request,result
+routes.get('/', (req,res) => res.render(views+ "index"))
+routes.get('/job', (req,res) => res.render(views+ "job"))
+routes.get('/job/edit', (req,res) => res.render(views +"job-edit"))
+routes.get('/profile', (req,res) => res.render(views + "profile", {profile}))
+
+
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+
+//usando template engine
+server.set('view engine', 'ejs')
+
+
+//habilitar arquivos estaticos
+server.use(express.static("public"))
+
+//route 
+server.use(routes)
+server.listen(3000, () => console.log("rodando o servidor.."))
+
+
