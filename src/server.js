3b1a4d2b8d87b493cbf5a2327a545df475ec38f8
dehveira <andrevieira_@outlook.com>
2021-03-31 01:08:37 -0300

    maratona-discovery aula-1

--- src/server.js
+++ src/server.js
@@ -13,4 +13,6 @@ server.use(express.static("public"))
 server.use(routes)
 server.listen(3000, () => console.log("rodando o servidor.."))
 
+//................................................................
+
 
