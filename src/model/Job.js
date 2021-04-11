6f56554fa7f208cfb16b1f324519c009f021c9cb
André Vieira <andrevieira_@outlook.com>
2021-04-10 21:03:06 -0300

    fim refatoraçao aula03

--- src/model/Job.js
+++ src/model/Job.js
@@ -22,6 +22,9 @@ module.exports = {
   update(newJobs) {
     data = newJobs;
   },
+  create(newJob) {
+    data.push(newJob);
+  },
   delete(id) {
     data = data.filter((job) => Number(job.id) !== Number(id));
   },
