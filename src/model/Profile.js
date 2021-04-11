4f83b80225288c894e1c96c2e48859c2428f5781
André Vieira <andrevieira_@outlook.com>
2021-04-11 19:15:19 -0300

    finalizado aula 4 banco de dados

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,19 +1,37 @@
-let data = {
-  name: "André Vieira",
-  avatar: "https://www.github.com/dehvieira.png",
-  "monthly-budget": 3000,
-  "hours-per-day": 6,
-  "days-per-week": 5,
-  vacation: 4,
-  "value-hour": 75,
-};
+const Database = require("../db/config");
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    const db = await Database();
+
+    const data = await db.get(`SELECT * FROM  profile`);
+
+    await db.close();
+
+    return {
+      name: data.name,
+      avatar: data.avatar,
+      monthly_budget: data.monthly_budget,
+      days_per_week: data.days_per_week,
+      "hours-per-day": data.hours_per_day,
+      vacation: data.vacation,
+      "value-hour": data.value_hour,
+    };
   },
 
-  update(newData) {
-    data = newData;
+  async update(newData) {
+    const db = await Database();
+
+    db.run(`UPDATE profile SET
+    name = "${newData.name}",
+    avatar = "${newData.avatar}",
+    monthly_budget = ${newData["monthly-budget"]},    
+    days_per_week = ${newData["days-per-week"]},
+    hours_per_day = ${newData["hours-per-day"]},
+    vacation = ${newData.vacation},
+    value_hour = ${newData["value-hour"]}
+    `);
+
+    await db.close();
   },
 };
